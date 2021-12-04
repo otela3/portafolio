e4d64e5451c6a5e1debb4019a5e3051a356f7738
otela3 <motelo@fabricadecodigo.cl>
2021-12-04 17:43:56 -0300

    primer test

--- main.py
+++ main.py
@@ -21,10 +21,11 @@ scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
 prediction_days = 60
+future_day = 30
 x_train, y_train = [], []
-for x in range(prediction_days, len(scaled_data)):
+for x in range(prediction_days, len(scaled_data)-future_day):
         x_train.append(scaled_data[x-prediction_days:x, 0])
-        y_train.append(scaled_data[x, 0])
+        y_train.append(scaled_data[x+future_day, 0])
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
